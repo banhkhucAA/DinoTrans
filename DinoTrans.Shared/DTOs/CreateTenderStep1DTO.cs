93bda34efcd9bb1216c64a37a6c1ac184711311b
an nguyen danh <annd@sphinxjsc.com>
2024-01-23 17:53:41 +0700

    đẩy lên để pull dc mới về

---
+++ DinoTrans.Shared/DTOs/CreateTenderStep1DTO.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinoTrans.Shared.DTOs
+{
+    public class CreateTenderStep1DTO
+    {
+        public DateOnly TenderStartDate { get; set; }
+        public DateOnly TenderEndDate { get; set;}
+        public TimeOnly TenderStartTime { get; set; }
+        public TimeOnly TenderEndTime { get; set; }
+    }
+}
