34c9af18e4b62fd1661af03358d307df501d292f
an nguyen danh <annd@sphinxjsc.com>
2024-01-30 11:17:10 +0700

    commit FE tạo máy mới

---
+++ DinoTrans.Shared/DTOs/ConstructionMachine/NewConstructionMachineDTO.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DinoTrans.Shared.DTOs.ConstructionMachine
+{
+    public class NewConstructionMachineDTO
+    {
+        public string Name { get; set; }
+        public string Brand { get; set; }
+        public string SerialNumber { get; set; }
+        public int CompanyShipperId { get; set; }
+        public string? Image { get; set; }
+        public float Length { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+        public float Weight { get; set; }
+    }
+}
